aa9026ab55820e6116799c4e409fbe6e45dadc26
Shreyas <slad0716@gmail.com>
2023-11-02 19:32:22 -0700

    feat: add statement selection inputs

--- src/components/add-button.tsx
+++ src/components/add-button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { PlusIcon } from 'lucide-react';
+import { Check, ChevronsUpDown, PlusIcon } from 'lucide-react';
 import { Button } from './ui/button';
 import {
   Dialog,
@@ -11,8 +11,46 @@ import {
   DialogTitle,
   DialogTrigger,
 } from './ui/dialog';
+import { Input } from './ui/input';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
+import { useState } from 'react';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from './ui/command';
+import { cn } from '@/lib/utils';
+import { Label } from './ui/label';
+
+const statementTypes = [
+  {
+    value: 'wf',
+    label: 'Wells Fargo',
+  },
+  {
+    value: 'citi',
+    label: 'CitiBank',
+  },
+  {
+    value: 'amex',
+    label: 'Amex',
+  },
+  {
+    value: 'applesavings',
+    label: 'Apple Savings',
+  },
+  {
+    value: 'applecard',
+    label: 'Apple Card',
+  },
+];
 
 export default function AddButton() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('');
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -24,11 +62,60 @@ export default function AddButton() {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Scan Statement</DialogTitle>
-          <DialogContent>
-            <DialogHeader>Select a document</DialogHeader>
-            <DialogDescription>This is a description</DialogDescription>
-          </DialogContent>
+          <DialogDescription>
+            Scan a bank or credit card statement
+          </DialogDescription>
         </DialogHeader>
+
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant={'outline'}
+              role="combobox"
+              aria-expanded={open}
+              className="justify-between"
+            >
+              {value
+                ? statementTypes.find((type) => type.value === value)?.label
+                : 'Select type...'}
+
+              <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+            </Button>
+          </PopoverTrigger>
+
+          <PopoverContent className="p-0">
+            <Command>
+              <CommandInput placeholder="Search types..." />
+              <CommandEmpty>No type found.</CommandEmpty>
+              <CommandGroup>
+                {statementTypes.map((type) => (
+                  <CommandItem
+                    key={type.value}
+                    value={type.value}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? '' : currentValue);
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        'mr-2 h-4 w-4',
+                        value === type.value ? 'opacity-100' : 'opacity-0'
+                      )}
+                    />
+                    {type.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </Popover>
+
+        <div className="grid w-full gap-1">
+          <Label htmlFor="file">Your statement</Label>
+          <Input type="file" placeholder="test" />
+        </div>
+
         <DialogFooter>
           <Button>Upload</Button>
         </DialogFooter>
