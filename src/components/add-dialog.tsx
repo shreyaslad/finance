6511182e7b900aa368c12f23cfdcf6a3761b63de
Shreyas <slad0716@gmail.com>
2023-11-03 11:53:59 -0700

    feat: statement upload button doesn't immediately close dialog

--- src/components/add-dialog.tsx
+++ src/components/add-dialog.tsx
@@ -9,13 +9,14 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
-  DialogClose,
 } from './ui/dialog';
 import { dialogOpenAtom } from '@/atoms/dialog-atom';
+import { useState } from 'react';
+import { Loader2 } from 'lucide-react';
 
 export default function AddDialog() {
   const [dialogOpen, setDialogOpen] = useAtom(dialogOpenAtom);
+  const [processing, setProcessing] = useState(false);
 
   return (
     <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
@@ -28,7 +29,22 @@ export default function AddDialog() {
         </DialogHeader>
 
         <DialogFooter>
-          <Button onClick={() => {}}>Upload</Button>
+          <Button
+            onClick={() => {
+              setProcessing(true);
+              setTimeout(() => {
+                setProcessing(false);
+                setDialogOpen(false);
+              }, 5000);
+            }}
+            disabled={processing}
+          >
+            {processing ? (
+              <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+            ) : null}
+
+            <span>{processing ? 'Uploading' : 'Upload'}</span>
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
