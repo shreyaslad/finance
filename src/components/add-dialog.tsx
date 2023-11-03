73f0ca769d5c652e92bf4cc795ce5057e5aaed94
Shreyas <slad0716@gmail.com>
2023-11-03 14:13:25 -0700

    feat: zod upload file validation

--- src/components/add-dialog.tsx
+++ src/components/add-dialog.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import { useAtom } from 'jotai';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import { Button } from './ui/button';
 import {
   Dialog,
@@ -23,6 +26,24 @@ import {
   SelectValue,
 } from './ui/select';
 import { Input } from './ui/input';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+
+const MAX_FILE_SIZE = 500000;
+const ACCEPTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/webp',
+];
 
 const statementTypes = [
   {
@@ -47,9 +68,36 @@ const statementTypes = [
   },
 ];
 
+// https://github.com/colinhacks/zod/issues/387#issuecomment-1191390673
+const formSchema = z.object({
+  statementType: z.string({
+    required_error: 'Please select a statement type.',
+  }),
+  statementFile: z
+    .custom<File[]>()
+    .refine((files) => ACCEPTED_IMAGE_TYPES.includes(files[0].type), {
+      message: 'Allowed types: .jpg, .jpeg, .png and .webp',
+    }),
+});
+
 export default function AddDialog() {
   const [dialogOpen, setDialogOpen] = useAtom(dialogOpenAtom);
   const [processing, setProcessing] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log('Statement type: ' + data.statementType);
+    console.log('File name: ' + data.statementFile[0].name);
+
+    setProcessing(true);
+    setTimeout(() => {
+      setProcessing(false);
+      setDialogOpen(false);
+    }, 5000);
+  }
 
   return (
     <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
@@ -61,38 +109,70 @@ export default function AddDialog() {
           </DialogDescription>
         </DialogHeader>
 
-        <Select>
-          <SelectTrigger>
-            <SelectValue placeholder="Select a source" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              {/* <SelectLabel>Sources</SelectLabel> */}
-              {statementTypes.map((item) => (
-                <SelectItem value={item.value}>{item.label}</SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="statementType"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Statement Type</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a source" />
+                      </SelectTrigger>
+                    </FormControl>
 
-        <Input type="file" />
+                    <SelectContent>
+                      {statementTypes.map((item) => (
+                        <SelectItem key={item.value} value={item.value}>
+                          {item.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
 
-        <Button
-          onClick={() => {
-            setProcessing(true);
-            setTimeout(() => {
-              setProcessing(false);
-              setDialogOpen(false);
-            }, 5000);
-          }}
-          disabled={processing}
-        >
-          {processing ? (
-            <Loader2 className="w-4 h-4 mr-2 animate-spin" />
-          ) : null}
+                  <FormDescription>
+                    Choose a statement type from one of the sources above.
+                  </FormDescription>
 
-          <span>{processing ? 'Uploading' : 'Upload'}</span>
-        </Button>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="statementFile"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Screenshot</FormLabel>
+                  <Input
+                    type="file"
+                    // accept={ACCEPTED_IMAGE_TYPES.join(' ')}
+                    {...form.register('statementFile', { required: true })}
+                  />
+
+                  <FormDescription>This is your statement.</FormDescription>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" disabled={processing}>
+              {processing ? (
+                <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+              ) : null}
+
+              <span>{processing ? 'Uploading' : 'Upload'}</span>
+            </Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
