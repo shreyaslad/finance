be27afc5fa336b29627a982ef66387ea6d2b6ece
Shreyas <slad0716@gmail.com>
2023-11-22 20:40:22 -0800

    feat: textarea and accept change ui

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 import { useAtom } from 'jotai';
 import { uploadAtom } from '@/atoms/upload-atom';
 
-import { MoveLeft } from 'lucide-react';
+import { Check, MoveLeft, X } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import UploadForm from '@/components/upload-form';
-import { Table } from '@/components/ui/table';
+import { Textarea } from '@/components/ui/textarea';
 
 export default function ScanPage() {
   const [uploadData, setUploadData] = useAtom(uploadAtom);
@@ -26,14 +26,40 @@ export default function ScanPage() {
         <h2 className="text-3xl font-semibold tracking-tight scroll-m-20 first:mt-0">
           Scan Statement
         </h2>
-        <p className="text-sm font-medium text-muted-foreground">
+        <p className="text-sm font-medium tracking-tight text-muted-foreground">
           Scan a bank or credit card statement
         </p>
       </div>
 
       <UploadForm />
 
-      {uploadData.length > 0 ? <p>Got data</p> : null}
+      {uploadData.length > 0 ? (
+        <div className="flex flex-col mt-4 gap-y-4">
+          <hr className="h-[2px] border-0 bg-muted" />
+          <h3 className="text-2xl font-semibold tracking-tight text-secondary-foreground scroll-m-20">
+            Your Expenses
+          </h3>
+
+          <Textarea
+            cols={100}
+            rows={20}
+            onChange={(e) => setUploadData(JSON.parse(e.target.value))}
+          >
+            {JSON.stringify(uploadData, null, 2)}
+          </Textarea>
+
+          <div className="flex flex-row gap-x-2">
+            <Button variant={'outline'} className="w-1/4 mb-4">
+              <X className="w-4 h-4 text-muted-foreground" />
+              {/* <span className="text-muted-foreground">Cancel</span> */}
+            </Button>
+            <Button className="w-full">
+              <Check className="w-4 h-4 mr-2" />
+              Yes, that looks right
+            </Button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
